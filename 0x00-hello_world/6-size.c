51777d207789f29062782789957050d467e99e5c
Duaa <DuaaJ2000@hotmail.com>
2023-08-10 19:50:20 -0700

    gh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,15 +10,10 @@
  */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
-printf("size of char: %lu byte(s)\n ",(unsigned long)sizeof(a));
-printf("size of int: %lu byte(s)\n ",(unsigned long)sizeof(b));
-printf("size of long int: %lu byte(s)\n ",(unsigned long)sizeof(c));
-printf("size of long long int: %lu byte(s)\n ",(unsigned long)sizeof(d));
-printf("size of float: %lu byte(s)\n ",(unsigned long)sizeof(f));
+printf("size of a", sizeof(char));
+printf("size of b", sizeof(int));
+printf("size of c", sizeof(long int));
+printf("size of d", sizeof(long long int))
+printf("size of f", sizeof(float));
 return (0);
 }
