a2d495bfa7b3539944666dd2446ff98c53054fa6
Duaa <DuaaJ2000@hotmail.com>
2023-08-11 14:47:56 -0700

    gh

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers of base 10 starting from 0
+ * Return: Always (sucess)
+ */
+
+int main(void)
+{
+int num = 0;
+
+while (num <= 0)
+{
+printf("%i", num);
+num++;
+}
+printf("\n");
+
+return (0);
+}
