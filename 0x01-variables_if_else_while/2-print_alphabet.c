eba8bcaf5c458725d2734d32fe87f60eb7353f61
Duaa <DuaaJ2000@hotmail.com>
2023-08-11 14:05:06 -0700

    kl

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print  the alphabet in lowercase
+ * Return: Always (sucess)
+ */
+
+int main(void)
+{
+
+char ch = 'a';
+while (ch <= 'z')
+{
+	putchar('\n');
+	ch++;
+}
+	return (0);
+}
