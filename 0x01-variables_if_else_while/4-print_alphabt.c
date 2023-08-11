6d16fa98d88335e259409db93e4945bdd55dabd6
Duaa <DuaaJ2000@hotmail.com>
2023-08-11 14:33:14 -0700

    ty

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print he alphabet in lowercase, and then in uppercase
+ * Return: Always (sucess)
+ */
+
+int main(void)
+{
+
+char ch = 'a';
+while (ch <= 'z')
+{
+if (ch == 'e' || ch == 'q')
+putchar(ch);
+ch++;
+}
+putchar('\n');
+
+return (0);
+}
