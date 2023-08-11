b19e1e407fbb2898beed5ba2f39e02ea892724cc
Duaa <DuaaJ2000@hotmail.com>
2023-08-10 19:23:51 -0700

    hj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A C program printf function
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");	
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
