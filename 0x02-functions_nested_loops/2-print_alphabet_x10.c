86bb049c133c1687419b24b162548758a7633beb
Duaa <DuaaJ2000@hotmail.com>
2023-08-15 13:24:47 -0700

    fg

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * prints 10 times the alphabet, in lowercase
+ */
+
+ void print_alphabet_x10(void)
+{
+	int line, c;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
