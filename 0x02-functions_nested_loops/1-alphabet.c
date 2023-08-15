84ff63789d69dd9cd30a1a157d8b53a0917531fb
Duaa <DuaaJ2000@hotmail.com>
2023-08-15 13:13:59 -0700

    alpa

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print the alphabet, in lowercase
+ * Return: Always (sucess)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+
+	return (0);
+}
