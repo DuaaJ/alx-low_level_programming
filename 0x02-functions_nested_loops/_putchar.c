2678fc95d251f034dc54b20dfe2aa3e4ed17dc9f
Duaa <DuaaJ2000@hotmail.com>
2023-08-15 12:49:44 -0700

    hj

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - print _putchar
+ * @ch: character to print
+ * Return: Always (sucess)
+ */
+
+int _putchar(char ch)
+{
+	return (write(1, &ch, 1));
+}
