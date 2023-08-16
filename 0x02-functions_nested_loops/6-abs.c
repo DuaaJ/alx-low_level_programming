484a45fd563a5836bee5894d44f2efc457ff8985
Duaa <DuaaJ2000@hotmail.com>
2023-08-15 19:11:45 -0700

    kl

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of number
+ *
+ * @num: number to computr
+ *
+ * Return: (success)
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+		num = (-1) * num;
+
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,6 +11,7 @@ void print_times_table(int n);
 void print_to_98(int n);
 int add(int, int);
 int print_sign(int n);
+int _abs(int);
 
 
 #endif
