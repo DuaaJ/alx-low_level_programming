60fcb16d7d9b67b935ecf9fa7b1932ff9d594e18
Duaa <DuaaJ2000@hotmail.com>
2023-08-15 18:53:17 -0700

    crazy

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * main - print the alphabet, in lowercase
- * Return: Always (sucess)
+ * print_alphabet -  prints the alphabet, in lowercase
+ *
  */
 
-int main(void)
+void print_alphabet(void)
 {
 	int c;
 
 	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
 	_putchar('\n');
-
-	return (0);
 }
