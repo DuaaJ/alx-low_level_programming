3ed0d6916f6a512e2bf9ca404d5555f7ce07ae5c
Duaa <DuaaJ2000@hotmail.com>
2023-08-15 18:12:03 -0700

    sec

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,7 +30,7 @@ void print_times_table(int n)
 				{
 					_putchar((res / 100) + 48);
 					_putchar((res / 10) % 10 + 48);
-				} else if (res <= 99 && res >= 19)
+				} else if (res <= 99 && res >= 10)
 					_putchar((res / 10) + 48);
 				_putchar((res % 10) + 48);
 			}
