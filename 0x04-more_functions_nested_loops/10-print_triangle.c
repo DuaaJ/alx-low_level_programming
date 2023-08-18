4eb9345b5d6c311666b19efb19e714e1acd78f3d
Duaa <DuaaJ2000@hotmail.com>
2023-08-18 06:47:23 -0700

    tie

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *  print_triangle - check the code
+ *
+ * @size: the size
+ *
+ *   Return: Always 0.
+ */
+
+void print_triangle(int size)
+{
+	int h, g;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (h = 0; h < size; h++)
+		{
+			for (g = 0; g < size; g++)
+			{
+				if ((h + g) <= size)
+					_putchar(' ');
+				else
+					_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
