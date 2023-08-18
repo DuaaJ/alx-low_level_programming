80e0c2e1249860f5930c532aacc8a27125a10d97
Duaa <DuaaJ2000@hotmail.com>
2023-08-18 02:52:42 -0700

    digitss

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,18 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9
+ * print_numbers -  prints the numbers, from 0 to 9
  *
- * Return: (sucess)
+ * Return: 0(success)
  */
 
 void print_numbers(void)
 {
-	char c;
+	char ch;
 
-	for (c = '0'; c <= '9'; c++)
+	for (ch = '0'; ch <= '9'; ch++)
 	{
-		_putchar(c + 48);
+		_putchar(ch + 48);
 	}
 	_putchar('\n');
 }
-
