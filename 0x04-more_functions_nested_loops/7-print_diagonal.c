3b73a1e4bced517ddbb32d7b3aec8c61c3bf4378
Duaa <DuaaJ2000@hotmail.com>
2023-08-18 05:20:22 -0700

    diag

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ *
+ * @n: umber of diag
+ *
+ * Return: 0(success)
+ *
+ */
+
+
+void print_diagonal(int n)
+{
+	int dia, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (dia = 1; dia <= n; dia++)
+		{
+			for (space = 1; space <= dia; space++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
