42dbf06ab355c5dc2b3b03c1c94bf117f8f48b57
Duaa <DuaaJ2000@hotmail.com>
2023-08-24 12:16:20 -0700

    ghj

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * leet - encodes a string into 1337.
+ * @st:the char
+ *
+ * Return: encode
+ */
+
+
+char *leet(char *st)
+{
+	char lett[] = {'A', 'E', 'O', 'T', 'L'};
+	int num[] = {'4', '3', '0', '7', '1'};
+	unsigned int i;
+
+	while (st)
+	{
+		for (i = 0; i < sizeof(lett) / sizeof(char); i++)
+		{
+			if (*st == lett[i] || *st == lett[i] + 32)
+			{
+				*st = 48 + num[i];
+			}
+		}
+		st++;
+	}
+	return (st);
+}
