472846a211ffb70790d8cafeaec9a2068457d061
Duaa <DuaaJ2000@hotmail.com>
2023-08-24 05:40:51 -0700

    hj

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include<stdio.h>
 
 /**
  * _strcat - concatenates two strings
@@ -13,9 +14,9 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, k;
 
 	strcat(dest, src);
+
 	return (dest);
 }
 
