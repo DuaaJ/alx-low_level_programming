7698ee5084b87f5edfc9fa56b25b272fc2a7e17c
Duaa <DuaaJ2000@hotmail.com>
2023-08-24 12:45:31 -0700

    fg

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_number - prints an integer
+ *
+ * @n: number
+ */
+
+void print_number(int n)
+{
+	unsigned  int nu;
+
+	if (nu < 0)
+	{
+		_putchar('-');
+		nu = -n;
+	}
+
+	if (nu / 10 != 0)
+	{
+		print_number(nu / 10);
+	}
+	_putchar((nu % 10) + '0');
+}
+
