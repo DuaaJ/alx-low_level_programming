41e7ea43c8f82c9ed826d1695b84440d80e1487f
Duaa <DuaaJ2000@hotmail.com>
2023-08-24 09:41:27 -0700

    dret

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: first string
+ * @s2:second string
+ *
+ * Return:0(same) 1(not same)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int value;
+
+	for (int i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			value = 1;
+		}
+		break;
+	}
+	if (value == 1)
+		return (1);
+	else
+		return (0);
+}
+
