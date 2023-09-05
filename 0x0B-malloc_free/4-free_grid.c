99e36484ad70e492586e7d50e44a0206f2ce5ddf
Duaa <DuaaJ2000@hotmail.com>
2023-09-05 14:29:14 -0700

    fhj

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,13 +3,15 @@
 /**
  * free_grid - frees a 2 dimensional grid
  *@grid: a grid
- @height: the height
+ * @height:the height
  */
 
 
 void free_grid(int **grid, int height)
 {
-	for (int i = 0; i < height; i++)
+	int i;
+
+	for (i = 0; i < height; i++)
 		free(grid[i]);
 	free(grid);
 }
