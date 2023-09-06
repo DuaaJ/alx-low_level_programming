54f9f8a8db26feb434f7bc0968c058d0726e9087
Duaa <DuaaJ2000@hotmail.com>
2023-09-06 16:16:13 -0700

    gh

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - rints its name
+ * @argc:number
+ * @argv:array
+ * Return:0
+ */
+
+int main(int argc, char const *'argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  adds positive numbers
+ * @argc:number
+ * @argc:array
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	char *ptr;
+	int sum = 0;
+
+	while (--argc)
+	{
+		for (ptr = argv[argc]; *ptr; ptr++)
+			if (*ptr < '0' || *ptr > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
