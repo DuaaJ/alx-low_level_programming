79ee29353b0fef8068fb32e8e04b163d00a0e6e8
Duaa <DuaaJ2000@hotmail.com>
2023-09-07 08:25:42 -0700

    78

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked -  allocates memory using
+ * @b: ccc
+ * Return: ss
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = malloc(b);
+
+	if (ptr == 0)
+		exit(98);
+
+	return (m);
+}
+
