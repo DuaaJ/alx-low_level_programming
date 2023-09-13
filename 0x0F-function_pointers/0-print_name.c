195700591abb3f6fdbff088fa83760da76263e87
Duaa <DuaaJ2000@hotmail.com>
2023-09-13 07:04:59 -0700

    dfg

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_name - print a name
+ * @name:the name
+ * @f:pointer to a function to print
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
