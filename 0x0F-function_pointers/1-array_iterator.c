f42d6c1317f13a0dcef5ea982237b81522481a3a
Duaa <DuaaJ2000@hotmail.com>
2023-09-13 07:33:29 -0700

    tyu

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array && size && action)
 	for (int i = 0; i < size; i++)
 	{
-		action(array[i]);
+		action(*array++);
 	}
 
 }
