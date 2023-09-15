04791058f3b42a11e5894bbbca884b8dc5838368
DuaaJ <DuaaJ2000@hotmail.com>
2023-09-15 15:40:43 -0700

    ert

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,92 @@
+#include "variadic_functions.h"
+
+
+/**
+ * f_s- ffgh
+ * @separator: hhhjjj
+ * @l:hjj
+ */
+
+void f_s(char *separator, va_list l)
+{
+	char *s= va_arg(l, char *);
+
+	switch ((int)(!s))
+		case 1: 
+			s = "(nil)";
+
+		printf("%s%s", separator, s);
+}
+
+/**
+ * f_f - ffgh
+ * @separator: hhhjjj
+ * @l:hjj
+ */
+
+void f_f(char *separator, va_list l)
+{
+	printf("%s%f", separator, va_arg(l, double));
+}
+
+/**
+ * f_char - ffgh
+ * @separator: hhhjjj
+ * @l:hjj
+ */
+
+void f_char(char *separator, va_list l)
+{
+        printf("%s%c", separator, va_arg(l, int));
+}
+
+/**
+ * f_int - ffgh
+ * @separator: hhhjjj
+ * @l:hjj
+ */
+
+void f_int(char *separator, va_list l)
+{
+        printf("%s%d", separator, va_arg(l, int));
+}
+/**
+ * print_all - print
+ * @format: the string
+ * @...: number
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i, j;
+	char separator = "";
+	va_list l;
+
+	fun_t func[] = {
+		{"s", f_s},
+		{"f", f_f},
+		{"i", f_int},
+		{"c", f_char},
+		{NULL, NULL}
+	};
+
+	va_start(l, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (func[j].funct)
+		{
+			if (format[i] == func[j].funct[0])
+			{
+				func[j].fa(separator, l);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(l);
+
+}
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,12 @@
 #include<stdio.h>
 #include<stdarg.h>
 
+typedef struct funct
+{
+	char *funct;
+	void (*fa)(char *, va_list);
+} fun_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
