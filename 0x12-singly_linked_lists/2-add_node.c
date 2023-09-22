fa5e83b7925af6d2fd9ec1be600c050f4cc70e04
DuaaJ <DuaaJ2000@hotmail.com>
2023-09-22 12:34:01 -0700

    ghj

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/**
+ * add_node - add node
+ * @head:the head of the node
+ * @str:the string
+ *
+ * Return:the address of the new
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp = malloc(sizeof(list_t));
+
+	if (!head || !temp)
+		return (NULL);
+	if (str)
+	{
+		temp->str = strdup(str);
+		if (temp->str)
+		{
+			free(temp);
+			return (NULL);
+		}
+		temp->len = strlen(str);
+	}
+	else
+	{
+		return (NULL);
+	}
+	temp->next = *head;
+	*head = temp;
+
+	return (temp);
+}
+
