c8b4999acd2850f457374906573bac354ec49e6b
DuaaJ <DuaaJ2000@hotmail.com>
2023-09-22 03:35:20 -0700

    ghj

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * print_list - print contain of list
+ * @h:head of linked list
+ * Return:number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t coun = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		coun++;
+		return (coun);
+	}
+}
