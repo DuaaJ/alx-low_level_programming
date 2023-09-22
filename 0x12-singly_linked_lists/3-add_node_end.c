84a60e3c9d7364ce90fcf9bae48b7d357ef8ec09
DuaaJ <DuaaJ2000@hotmail.com>
2023-09-22 10:01:46 -0700

    fgh

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,37 @@
+#include"lists.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/**
+ * add_node_end - add a nodeat the end
+ * @head:the start of node
+ * @str:string to add
+ *
+ * Return:address of the added node
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp = malloc(sizeof(list_t));
+	list_t *ptr = *head;
+
+	if (!head || !temp)
+		return (NULL);
+	if (str)
+	{
+		temp->str = strdup(str);
+		temp->next = NULL;
+
+		while (ptr->next != NULL)
+		{
+			ptr = ptr->next;
+		}
+		ptr->next = temp;
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (temp);
+}
